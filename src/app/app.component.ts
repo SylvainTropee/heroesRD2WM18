6b41b638f9d9fcf26a25d05b16b2054223fbb824
stropee <stropee@campus-eni.fr>
2024-10-03 17:04:56 +0200

    installation Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TitleComponent} from "./components/title/title.component";
+import {AllHeroesComponent} from "./components/all-heroes/all-heroes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleComponent, AllHeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/all-heroes/all-heroes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-heroes',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-heroes.component.html',
+  styleUrl: './all-heroes.component.css'
+})
+export class AllHeroesComponent {
+
+}
